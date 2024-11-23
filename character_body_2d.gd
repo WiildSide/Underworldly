1a598c09b41e71560a5fb7aac4f486a6a4db6c3b
Ricky Ward <rward0079@outlook.com>
2024-11-23 15:11:31 -0500

    added double jump and various other physics

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,9 +1,42 @@
 extends CharacterBody2D
 @export var speed = 400
-func get_input():
-	var input_direction = Input.get_vector("left","right","up","down")
-	velocity = input_direction * speed
+const JUMP_VELOCITY = -600.0
+var jumps: int = 2
+#func get_input():
+	#var input_direction = Input.get_vector("left","right","up","down")
+	#velocity = input_direction * speed
 
-func _physics_process(delta):
-	get_input()
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta * 1.25
+		# Handles dropdown movement
+		if Input.is_action_pressed("down"):
+			velocity += get_gravity() * delta * 2
+	elif is_on_floor():
+		jumps = 2
+	
+	#  Handles jumping
+	if Input.is_action_just_pressed("up") and jumps > 0:
+		#var input_direction = Input.get_vector("left","right","up","down")
+		#velocity = input_direction * speed
+		velocity.y = JUMP_VELOCITY
+		jumps -= 1
+		
+	# Handles left-right movement
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	# Handles crouching and falling
+	#if Input.is_action_just_pressed("down")
+		
+
+		
+		
+		
+	
+	#get_input()
 	move_and_slide()
