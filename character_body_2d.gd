fa39a28ee194a2f1deab4e1af943dade2fe8ce36
Ricky Ward <rward0079@outlook.com>
2024-11-23 16:24:46 -0500

    implemented half-working dash, added walking animation for angel

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 @export var speed = 400
 const JUMP_VELOCITY = -600.0
 var jumps: int = 2
+var dash_ready: bool = true
+var dashing_speed: int = 1
+
 #func get_input():
 	#var input_direction = Input.get_vector("left","right","up","down")
 	#velocity = input_direction * speed
@@ -26,17 +29,33 @@ func _physics_process(delta: float) -> void:
 	# Handles left-right movement
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction * speed * dashing_speed
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		
+		
 	# Handles crouching and falling
 	#if Input.is_action_just_pressed("down")
+	
+	# Handles dashing
+	if Input.is_action_just_pressed("dash") and dash_ready:
+		dash_ready = false
+		$DashCooldown.start()
+	if $DashCooldown.time_left > ($DashCooldown.wait_time - 0.5):	
+		dashing_speed = 3
+	else:
+		dashing_speed = 1
 		
-
-		
+	
+	
+	
 		
 		
 	
 	#get_input()
 	move_and_slide()
+
+
+func _on_dash_cooldown_timeout() -> void:
+	dash_ready = true
