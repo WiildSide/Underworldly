86c4837b197eecee16357e8d6f9da0c5752b0052
Ricky Ward <rward0079@outlook.com>
2024-11-23 20:57:09 -0500

    fixed dashes, fixed backround sorta

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -4,48 +4,47 @@ const JUMP_VELOCITY = -600.0
 var jumps: int = 2
 var dash_ready: bool = true
 var dashing_speed: int = 1
+var last_direction: int = 1
 
 #func get_input():
 	#var input_direction = Input.get_vector("left","right","up","down")
 	#velocity = input_direction * speed
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta * 1.25
-		# Handles dropdown movement
-		if Input.is_action_pressed("down"):
-			velocity += get_gravity() * delta * 2
-	elif is_on_floor():
-		jumps = 2
-	
-	#  Handles jumping
-	if Input.is_action_just_pressed("up") and jumps > 0:
-		#var input_direction = Input.get_vector("left","right","up","down")
-		#velocity = input_direction * speed
-		velocity.y = JUMP_VELOCITY
-		jumps -= 1
+	if not $DashCooldown.time_left > ($DashCooldown.wait_time - 0.5):
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta * 1.25
+			# Handles dropdown movement
+			if Input.is_action_pressed("down"):
+				velocity += get_gravity() * delta * 2
+		elif is_on_floor():
+			jumps = 2
 		
-	# Handles left-right movement
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * speed * dashing_speed
-		
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		
-		
-	# Handles crouching and falling
-	#if Input.is_action_just_pressed("down")
+		#  Handles jumping
+		if Input.is_action_just_pressed("up") and jumps > 0:
+			velocity.y = JUMP_VELOCITY
+			jumps -= 1
+			
+		# Handles left-right movement
+		var direction = Input.get_axis("left", "right")
+		if direction:
+			last_direction = direction
+			velocity.x = direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			
+			
+		# Handles crouching and falling
+		#if Input.is_action_just_pressed("down")
 	
 	# Handles dashing
 	if Input.is_action_just_pressed("dash") and dash_ready:
 		dash_ready = false
 		$DashCooldown.start()
-	if $DashCooldown.time_left > ($DashCooldown.wait_time - 0.5):	
-		dashing_speed = 3
-	else:
-		dashing_speed = 1
+	if $DashCooldown.time_left > ($DashCooldown.wait_time - 0.5):
+		velocity.x = last_direction * speed * 3
+		velocity.y = 0
 		
 	# animations
 	if dashing_speed > 1:
